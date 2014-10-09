be8ab7c075a46e3c8e3c10710b784d007a218acb
Matt Smith <matt24smith@gmail.com>
2014-10-09 19:55:14 -0300

    Update project1.java
    Told users to read books. users are dumb.

--- project1.java
+++ project1.java
@@ -1,12 +1,19 @@
 /*
-*Java Project 1
-*
-*/
-
-
+ * CSCI 1100 - Project 1
+ * Educational Game
+ * Due Date: 2014/10/27 
+ * 
+ * Authors:
+ * Matt Smith
+ * Becky Ansems
+ * Pascha Love-Protter
+ * Adbullah Negahban
+ * 
+ */
 public class project1 {
 
 	public static void main(String[] args) {
+		System.out.println("learn how to do things and open a book.\nEnd of game.");
 
 	}
 
